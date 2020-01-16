2d5789409c747f9bde100f0393580da4b32844b4
Your Name <grizzlyadam@Adams-MacBook-Pro.local>
2020-01-15 23:43:22 -0800

    Initial setup of the file and folder structure for this project.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Double Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Guacamole Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Beyond Meat Burger");
